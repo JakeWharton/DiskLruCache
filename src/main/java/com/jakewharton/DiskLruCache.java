JakeWharton/DiskLruCache

commit d771a5bd9ad2e30bd46be6c320cab4d8dd3b0252

* Support keys which need URL encoding.

src/main/java/com/jakewharton/DiskLruCache.java
@@ -33,8 +33,10 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.lang.reflect.Array;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -886,7 +888,11 @@ public final class DiskLruCache implements Closeable {
         private long sequenceNumber;
 
         private Entry(String key) {
-            this.key = key;
+	        try {
+                this.key = URLEncoder.encode(key, UTF_8.name());
+            } catch (UnsupportedEncodingException uee) {
+                throw new AssertionError(uee.getMessage());
+            }
             this.lengths = new long[valueCount];
         }
 

src/test/java/com/jakewharton/DiskLruCacheTest.java
@@ -80,6 +80,21 @@ public final class DiskLruCacheTest extends TestCase {
         assertEquals("DE", snapshot.getString(1));
     }
 
+    public void testWriteAndReadEncodedEntry() throws Exception {
+        DiskLruCache.Editor creator = cache.edit("https://graph.facebook.com/me");
+        creator.set(0, "{ME!}");
+        creator.set(1, "{profile}");
+        assertNull(creator.getString(0));
+        assertNull(creator.newInputStream(0));
+        assertNull(creator.getString(1));
+        assertNull(creator.newInputStream(1));
+        creator.commit();
+
+        DiskLruCache.Snapshot snapshot = cache.get("https://graph.facebook.com/me");
+        assertEquals("{ME!}", snapshot.getString(0));
+        assertEquals("{profile}", snapshot.getString(1));
+    }
+
     public void testReadAndWriteEntryAcrossCacheOpenAndClose() throws Exception {
         DiskLruCache.Editor creator = cache.edit("k1");
         creator.set(0, "A");

commit a1462941e43b54660e2174fdf6bcb22fcc24d994

* Correcting checkstyle errors introduced in d771a5bd9ad2e30bd46be6c320cab4d8dd3b0252 to fix Issue #8.

src/main/java/com/jakewharton/DiskLruCache.java
@@ -888,7 +888,7 @@ public final class DiskLruCache implements Closeable {
         private long sequenceNumber;
 
         private Entry(String key) {
-	        try {
+            try  {
                 this.key = URLEncoder.encode(key, UTF_8.name());
             } catch (UnsupportedEncodingException uee) {
                 throw new AssertionError(uee.getMessage());